6a4481ee8b6beabdc6f674c12e9e8b5599e6e61a
David Bausela <david.bausela@alumnos.upm.es>
2023-03-31 16:28:14 +0200

    refactor, red

--- src/test/java/usantatecla/MaxTest.java
+++ src/test/java/usantatecla/MaxTest.java
@@ -1,5 +1,6 @@
 package usantatecla;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,13 @@ import org.junit.jupiter.api.Test;
 class MaxTest {
    
   @Test
-  void givenIntervalWhenIncludeWithIncludedValueThenTrue(){
+  public void givenMaxWhenGreaterWithLessValueThenTrue(){
     assertTrue(new Max(true, 4).greater(0.0));
+  }
+
+  @Test
+  void givenMaxWhenGreaterWithLessValueThenFalse(){
+    assertFalse(new Max(true, 4).greater(5.0));
   }
  
 }
