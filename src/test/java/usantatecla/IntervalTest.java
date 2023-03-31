a35abc67be0c757e7a14a8ba5449f4f9e119c959
David Bausela <dbausela@gmail.com>
2023-03-31 12:16:27 +0200

    red, open/closed intervals, limit value

--- src/test/java/usantatecla/IntervalTest.java
+++ src/test/java/usantatecla/IntervalTest.java
@@ -9,12 +9,16 @@ class IntervalTest {
   
   @Test
   void givenIntervalWhenIncludeWithIncludedValueThenTrue(){
-    assertTrue(new Interval(-1.7,5555.0).include(0.0));
+    assertTrue(new Interval(true, -1.7, true, 5555.0).include(0.0));
   }
 
   @Test
   void givenIntervalWhenIncludeWithNotIncludedValueThenFalse(){
-    assertFalse(new Interval(-1.7,5555.0).include(9000.0));
+    assertFalse(new Interval(true, -1.7, true, 5555.0).include(9000.0));
   }
-  
+
+  @Test
+  void givenIntervalWhenIncludeWithLimitValueThenFalse(){
+    assertFalse(new Interval(true, -1.7,true, 5555.0).include(5555.0));
+  }
 }
