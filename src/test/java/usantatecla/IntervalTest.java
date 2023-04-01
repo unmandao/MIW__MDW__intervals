5a09b8e6266201210dfb2ddb4839ef09b53c349f
David Bausela <david.bausela@alumnos.upm.es>
2023-04-01 10:59:08 +0200

    refactor, IntervalTest (this status corresponds to commit c3786c1a of the original repository)

--- src/test/java/usantatecla/IntervalTest.java
+++ src/test/java/usantatecla/IntervalTest.java
@@ -10,30 +10,10 @@ class IntervalTest {
     @Test
     void givenIntervalOpenOpenWhenIncludeWithIncludedValueThenTrue() {
         assertTrue(new Interval(true, -1.7, new Max(5555.0)).include(0.0));
-    }
-
-    @Test
-    void givenIntervalOpenOpenWhenIncludeWithNotIncludedValueThenFalse() {
         assertFalse(new Interval(true, -1.7, new Max(5555.0)).include(9000.0));
-    }
-
-    @Test
-    void givenIntervalOpenOpenWhenIncludeWithLimitValueThenFalse() {
         assertFalse(new Interval(true, -1.7, new Max(5555.0)).include(5555.0));
-    }
-
-    @Test
-    void givenIntervalOpenCloseWhenIncludeWithIncludedValueThenTrue() {
         assertTrue(new Interval(true, -1.7, new ClosedMax(5555.0)).include(0.0));
-    }
-
-    @Test
-    void givenIntervalOpenCloseWhenIncludeWithNotIncludedValueThenFalse() {
         assertFalse(new Interval(true, -1.7, new ClosedMax(5555.0)).include(9000.0));
-    }
-
-    @Test
-    void givenIntervalOpenCloseWhenIncludeWithLimitValueThenTrue() {
         assertTrue(new Interval(true, -1.7, new ClosedMax(5555.0)).include(5555.0));
     }
 
