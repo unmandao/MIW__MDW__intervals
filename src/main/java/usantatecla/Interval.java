956090fe4a48e661df72acf5868a55925113a24a
David Bausela <david.bausela@alumnos.upm.es>
2023-04-01 09:58:23 +0200

    refactor, refactor, refactor without open boolean

--- src/main/java/usantatecla/Interval.java
+++ src/main/java/usantatecla/Interval.java
@@ -2,17 +2,17 @@ package usantatecla;
 
 public class Interval {
 
-	private double min;
-	private Max max;
+    private double min;
+    private Max max;
 
-	public Interval(boolean minOpen, double min, Max max) {
-		assert min <= max.value;
-		this.min = min;
-		this.max = max;
-	}
+    public Interval(boolean minOpen, double min, Max max) {
+        assert min <= max.value;
+        this.min = min;
+        this.max = max;
+    }
 
-	public boolean include(double value) {
-			return this.min <= value && this.max.greaterOrEquals(value);
-	}
+    public boolean include(double value) {
+        return this.min <= value && this.max.isOnLeft(value);
+    }
 
 }
--- src/test/java/usantatecla/IntervalTest.java
+++ src/test/java/usantatecla/IntervalTest.java
@@ -6,19 +6,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class IntervalTest {
-  
-  @Test
-  void givenIntervalWhenIncludeWithIncludedValueThenTrue(){
-    assertTrue(new Interval(true, -1.7, new Max(5555.0, true)).include(0.0));
-  }
 
-  @Test
-  void givenIntervalWhenIncludeWithNotIncludedValueThenFalse(){
-    assertFalse(new Interval(true, -1.7, new Max(5555.0, true)).include(9000.0));
-  }
+    @Test
+    void givenIntervalWhenIncludeWithIncludedValueThenTrue() {
+        assertTrue(new Interval(true, -1.7, new Max(5555.0)).include(0.0));
+    }
 
-  @Test
-  void givenIntervalWhenIncludeWithLimitValueThenFalse(){
-    assertFalse(new Interval(true, -1.7, new Max(5555.0, true)).include(5555.0));
-  }
+    @Test
+    void givenIntervalWhenIncludeWithNotIncludedValueThenFalse() {
+        assertFalse(new Interval(true, -1.7, new Max(5555.0)).include(9000.0));
+    }
+
+    @Test
+    void givenIntervalWhenIncludeWithLimitValueThenFalse() {
+        assertFalse(new Interval(true, -1.7, new Max(5555.0)).include(5555.0));
+    }
 }
